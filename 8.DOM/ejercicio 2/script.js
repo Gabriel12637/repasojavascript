b86eff4f3eb21bccfba19c2753bd3274bcfae52e
Gabriel12637 <sano60151@gmail.com>
2025-10-16 16:48:44 -0500

    clase

--- 8.DOM/ejercicio 2/script.js
+++ 8.DOM/ejercicio 2/script.js
@@ -1,5 +1,6 @@
-let titulo = document.querySelector("h1")
-let primertitulo = titulo.textContent
-titulo.textContent="otro titulo"
-let contenido = document.querySelector("#contenido")
-contenido.innerHTML=` <p> ${primertitulo} </p>`
+let titulo = document.querySelector('h1');
+titulo.textContent = "Otro titulo";
+let primerTitulo = titulo.textContent;
+titulo.textContent = "Mi primer titulo";
+let contenido = document.querySelector('#contenido');
+contenido.innerHTML = `<p>${titulo.textContent}</p>`;
